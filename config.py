1bc61df30f14473c75e90f17bdc46f669c763dee
rishurajgit <rajrishu.cs@gmail.com>
2026-06-24 02:26:21 +0530

    Added two different OpenRouter Model

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     OPENROUTER_API_KEY: str
-    OPENROUTER_MODEL: str
+    OPENROUTER_MODEL_1: str
+    OPENROUTER_MODEL_2: str
     
     model_config = SettingsConfigDict(
     env_file=".env",
