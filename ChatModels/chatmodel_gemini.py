06afc09b3fe88223a742aec6e44707e2635a9d31
rishurajgit <rajrishu.cs@gmail.com>
2026-06-30 16:46:15 +0530

    Gemini Model Setup

--- ChatModels/chatmodel_gemini.py
+++ ChatModels/chatmodel_gemini.py
@@ -1,11 +1,11 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from config import settings
 
-llm = ChatGoogleGenerativeAI(
+chatmodels = ChatGoogleGenerativeAI(
     model=settings.GEMINI_CHAT_MODEL,
     google_api_key=settings.GEMINI_API_KEY
 )
 
-result = llm.invoke("What is the captial of India?")
+result = chatmodels.invoke("What is the captial of India?")
 
 print(result.content)
